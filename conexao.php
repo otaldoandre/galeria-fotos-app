f025a0056fa695db22c5f71c85c782e531f17c93
EloyChavo <andrealves431@hotmail.com>
2023-07-29 22:21:43 -0300

    Modify connection php file to match heroku's MySQL ClearDB

--- conexao.php
+++ conexao.php
@@ -1,9 +1,12 @@
 <?php
-	$host = "localhost"; 			
-	$user = "root"; 
-	$pass = "usbw"; 
-	$banco = "banco_fotografias";
-		
-	$conexao = @mysqli_connect($host, $user, $pass, $banco ) 
-	or die ("Problemas com a conexão do Banco de Dados");
+	//Get Heroku ClearDB connection information
+	$cleardb_url = parse_url(getenv("CLEARDB_DATABASE_URL"));
+	$cleardb_server = $cleardb_url["host"];
+	$cleardb_username = $cleardb_url["user"];
+	$cleardb_password = $cleardb_url["pass"];
+	$cleardb_db = substr($cleardb_url["path"],1);
+	$active_group = 'default';
+	$query_builder = TRUE;
+	// Connect to DB
+	$conn = mysqli_connect($cleardb_server, $cleardb_username, $cleardb_password, $cleardb_db);
 ?>
